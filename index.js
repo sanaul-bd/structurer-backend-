5b23db8cd7b98258e66e9d594f9f28d5447e22ca
sanaul-bd <sanaullah.bd21@gmail.com>
2024-07-02 20:46:52 +0600

    server is running

--- index.js
+++ index.js
@@ -25,23 +25,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        // *
-        app.get('/data', async (req, res) => {
-            try {
-                // const data = await collection.find({}).toArray();
-                // res.status(200).json(data);
-            } catch (error) {
-                res.status(500).json({ error: 'Failed to fetch data' });
-            }
-        });
-        // *
+        app.get("/", (req, res) => {
+            res.send(`Server is runnig on ${port} Port `)
+        })
 
         app.listen(port, () => {
             console.log(`Server is running on ::> http://localhost:${port}`);
